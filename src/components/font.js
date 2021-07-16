1d7b57599d97b425ad28225513ef5287d332f145
Aman <guptaaman200115@gmail.com>
2021-07-16 13:14:20 +0530

    Adding new font asset and configure

--- src/components/font.js
+++ src/components/font.js
@@ -1,4 +1,5 @@
 import '../assets/font/Inter-Medium.ttf';
 import '../assets/font/Inter-Regular.ttf';
 import '../assets/font/Inter-SemiBold.ttf';
+import '../assets/font/PaytoneOne-Regular.ttf'
 import './font.css';
