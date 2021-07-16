9b02560d8b583b2071857fdbf5beefd85a7d38b9
Aman <guptaaman200115@gmail.com>
2021-07-16 13:23:01 +0530

    Button Stories Added

--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -3,14 +3,14 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import {View,Text} from 'react-native'
 import {Button} from '../components/button';
+import { color } from '../components/style';
 
 storiesOf('Button', module)
   .addDecorator(story => (
     <View>{story()}</View>
   ))
-  .add('Default Button', () => (
-    <Button
-      onPress={action('clicked')}
-    //   children = {<Text>Next</Text>}
-    />
-  ))
+  .add('Btn1', () => (<Button onPress={action('clicked')} style={{backgroundColor : color.orange}}/>))
+  .add('Btn1 Disabled', () => (<Button onPress={action('clicked')} disabled style={{backgroundColor : color.orange}}/>))
+  .add('Btn2', () => (<Button onPress={action('clicked')} style={{backgroundColor : color.blue}}/>))
+  .add('Btn2 Disabled', () => (<Button onPress={action('clicked')} disabled style={{backgroundColor : color.blue}}/>));
+  
