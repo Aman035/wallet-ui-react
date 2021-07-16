3ab186c97b935bf2ae306c1f4d50e9cfacc1694f
Aman <guptaaman200115@gmail.com>
2021-07-16 13:22:34 +0530

    Button Changes

--- src/components/button.js
+++ src/components/button.js
@@ -4,25 +4,28 @@ import {
   StyleSheet
 } from 'react-native';
 import PropTypes from 'prop-types';
+import { H3Text } from './text';
+import { color } from './style';
 
 const styles = StyleSheet.create({
   touchable: {
     justifyContent: 'center',
     alignItems: 'center',
     minHeight: 60,
-    backgroundColor : '#F7931A',
     minWidth: 60,
     zIndex: 10,
-  },
+    borderRadius : '5px'
+  }
 });
 
-export const Button = ({ onPress, disabled, children}) => (
+export const Button = ({ onPress, disabled, children='Btn Text',style}) => (
   <TouchableOpacity
-    style={[{ opacity: disabled ? 0.5 : 1 }, styles.touchable]}
+    style={[{ opacity: disabled ? 0.5 : 1 }, styles.touchable , style]}
     disabled={disabled}
     onPress={onPress}
   >
-    {children}
+    <H3Text style={{color : color.white}}>{children}</H3Text>
+    
   </TouchableOpacity>
 );
 
