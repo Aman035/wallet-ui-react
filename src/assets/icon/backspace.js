b5c0d4e242547d8e12617d389f9f818f176288f1
Aman <guptaaman200115@gmail.com>
2021-07-18 20:08:30 +0530

    New Icon Asset and Story

---
+++ src/assets/icon/backspace.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function BackIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="18"
+      fill="none"
+      viewBox="0 0 24 18"
+      {...props}
+    >
+      <path
+        fill="#000"
+        fillRule="evenodd"
+        d="M6.243 0a2 2 0 00-1.679.913L.321 7.472A2 2 0 00.269 9.56l4.309 7.442a2 2 0 001.73.998h14.847a2 2 0 002-2V2a2 2 0 00-2-2H6.243zm2.464 5.308a1 1 0 011.414-1.415l3.543 3.543 3.528-3.528a1 1 0 111.415 1.414L15.078 8.85l3.529 3.529a1 1 0 01-1.415 1.414l-3.528-3.528-3.543 3.543a1 1 0 01-1.414-1.415L12.25 8.85 8.707 5.308z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default BackIcon;
--- src/stories/icon.stories.js
+++ src/stories/icon.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../components/main-content';
-import MatressIcon from '../assets/img/Logo'
+import MatressIcon from '../assets/img/Logo';
+import Backspace from '../assets/icon/backspace';
 
 storiesOf('Icons', module)
   .addDecorator(story => (
       <MainContent style={{ justifyContent: 'center' }}>{story()}</MainContent>
   ))
-  .add('Matress', () => <MatressIcon/>);
+  .add('Matress', () => <MatressIcon/>)
+  .add('Backspace' , () => <Backspace/>);
