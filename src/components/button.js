1dd06e61a19841c5907b683dedf869109fe75a93
Aman <guptaaman200115@gmail.com>
2021-07-18 19:53:58 +0530

    Adding New Button and Story

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  TouchableOpacity,
-  StyleSheet
-} from 'react-native';
+import {TouchableOpacity,StyleSheet,ViewPropTypes} from 'react-native';
 import PropTypes from 'prop-types';
 import { H3Text } from './text';
 import { color } from './style';
@@ -13,24 +10,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     minHeight: 60,
     minWidth: 60,
-    zIndex: 10,
+  },
+});
+
+export const Button = ({ onPress, disabled, children = '1', style }) => (
+  <TouchableOpacity
+    style={[{ opacity: disabled ? 0.5 : 1 }, styles.touchable, style]}
+    disabled={disabled}
+    onPress={onPress}
+  >
+    <H3Text>{children}</H3Text>
+  </TouchableOpacity>
+);
+
+Button.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  children: PropTypes.string,
+  style: ViewPropTypes.style,
+};
+
+const styles2 = StyleSheet.create({
+  touchable: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf : 'stretch',
+    minHeight: 60,
+    minWidth: 60,
     borderRadius : '5px'
   }
 });
 
-export const Button = ({ onPress, disabled, children='Btn Text',style}) => (
+export const Button2 = ({ onPress, disabled, children='Btn Text',style}) => (
   <TouchableOpacity
-    style={[{ opacity: disabled ? 0.5 : 1 }, styles.touchable , style]}
+    style={[{ opacity: disabled ? 0.5 : 1 }, styles2.touchable , style]}
     disabled={disabled}
     onPress={onPress}
   >
     <H3Text style={{color : color.white}}>{children}</H3Text>
-    
   </TouchableOpacity>
 );
 
-// Button.propTypes = {
-//   onPress: PropTypes.func.isRequired,
-//   disabled: PropTypes.bool,
-//   children: PropTypes.node,
-// };
+Button.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  children: PropTypes.string,
+};
+
+export default Button;
--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import {View,Text} from 'react-native'
-import {Button} from '../components/button';
+import {Button2} from '../components/button';
 import { color } from '../components/style';
+import Button from '../components/button';
+import MainContent from '../components/main-content';
 
 storiesOf('Button', module)
   .addDecorator(story => (
-    <View>{story()}</View>
+    <MainContent>{story()}</MainContent>
   ))
-  .add('Btn1', () => (<Button onPress={action('clicked')} style={{backgroundColor : color.orange}}/>))
-  .add('Btn1 Disabled', () => (<Button onPress={action('clicked')} disabled style={{backgroundColor : color.orange}}/>))
-  .add('Btn2', () => (<Button onPress={action('clicked')} style={{backgroundColor : color.blue}}/>))
-  .add('Btn2 Disabled', () => (<Button onPress={action('clicked')} disabled style={{backgroundColor : color.blue}}/>));
+  .add('Default Btn', () => (<Button onPress = {action('clicked')} style={{borderStyle : 'solid' , borderColor : '#000000' , borderWidth : '1px'}}/>))
+  .add('Btn1', () => (<Button2 onPress={action('clicked')} style={{backgroundColor : color.orange}}/>))
+  .add('Btn1 Disabled', () => (<Button2 onPress={action('clicked')} disabled style={{backgroundColor : color.orange}}/>))
+  .add('Btn2', () => (<Button2 onPress={action('clicked')} style={{backgroundColor : color.blue}}/>))
+  .add('Btn2 Disabled', () => (<Button2 onPress={action('clicked')} disabled style={{backgroundColor : color.blue}}/>));
   
