2df14eefbe816d7369c21b06ba5416e7d73be355
Aman <guptaaman200115@gmail.com>
2021-07-20 00:16:11 +0530

    Pin Story Changes

--- src/stories/pin.stories.js
+++ src/stories/pin.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import {PinBubbles, PinKeyboard} from '../components/pin';
+import {PinSecret, PinPad} from '../components/pin';
 import MainContent from '../components/main-content';
 
 storiesOf('Pin', module)
   .addDecorator(story => (
       <MainContent style={{ justifyContent: 'center' }}>{story()}</MainContent>
   ))
-  .add('Pin Pad', () => <PinKeyboard onInput={action('input digit')} onBackspace={action('backspace')}/>)
-  .add('Pin Bubbles' , () => <PinBubbles pin="1234"/>);
+  .add('Pin Pad', () => <PinPad onInput={action('input digit')} onBackspace={action('backspace')}/>)
+  .add('Pin Secret' , () => <PinSecret pin="12"/>);
