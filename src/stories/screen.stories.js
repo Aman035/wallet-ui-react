91d22f69b0422262d52917061e30b3cf19339b8f
Aman <guptaaman200115@gmail.com>
2021-07-22 18:13:50 +0530

    New Stories Added

--- src/stories/screen.stories.js
+++ src/stories/screen.stories.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {Cover} from '../view/cover';
 import ChoosePin from '../view/choose_pin';
+import ConfirmPin from '../view/confirm_pin';
 
 storiesOf('Screens', module)
 .add('Cover', () => <Cover/>)
 .add('Choose Pin', () => (<ChoosePin/>))
+.add('Confirm Pin' , () => (<ConfirmPin/>));
--- src/stories/text.stories.js
+++ src/stories/text.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../components/main-content';
 import {View} from 'react-native';
-import {H1Text , H2Text,  H3Text , H4Text , H5Text} from '../components/text';
+import {H1Text , H2Text,  H3Text , H4Text , H5Text , H6Text} from '../components/text';
 
 storiesOf('Text', module)
   .addDecorator(story => (
@@ -13,4 +13,5 @@ storiesOf('Text', module)
   .add('Heading 2', () => <H2Text>Heading 2</H2Text>)
   .add('Heading 3', () => <H3Text>Heading 3</H3Text>)
   .add('Heading 4', () => <H4Text>Heading 4</H4Text>)
-  .add('Heading 5', () => <H5Text>Heading 5</H5Text>);
+  .add('Heading 5', () => <H5Text>Heading 5</H5Text>)
+  .add('Heading 6' ,() => <H6Text>Heading 6</H6Text>);
