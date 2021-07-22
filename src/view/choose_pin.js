65cf20e6b7426ae1b27e1c65955670fa4ece74b2
Aman <guptaaman200115@gmail.com>
2021-07-22 18:14:51 +0530

    Minor Changes

--- src/view/choose_pin.js
+++ src/view/choose_pin.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import { View ,StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
 import MainContent from '../components/main-content';
-import { H1Text ,H3Text , H6Text} from '../components/text';
+import {H3Text , H6Text} from '../components/text';
 import { PinSecret, PinPad } from '../components/pin';
 import { color } from '../components/style';
 
-//
-// Set Pin View (Mobile)
-//
-
+//choose Pin View
 const styles = StyleSheet.create({
   content: {
     paddingLeft: 20,
