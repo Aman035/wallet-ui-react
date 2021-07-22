09ce6aa45d966d63caefd106a990cc68b9b63a06
Aman <guptaaman200115@gmail.com>
2021-07-22 18:35:56 +0530

    Login Story

--- src/stories/screen.stories.js
+++ src/stories/screen.stories.js
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react';
 import {Cover} from '../view/cover';
 import ChoosePin from '../view/choose_pin';
 import ConfirmPin from '../view/confirm_pin';
+import LoginPin from '../view/login';
 
 storiesOf('Screens', module)
 .add('Cover', () => <Cover/>)
 .add('Choose Pin', () => (<ChoosePin/>))
-.add('Confirm Pin' , () => (<ConfirmPin/>));
+.add('Confirm Pin' , () => (<ConfirmPin/>))
+.add('Login Pin' , () => (<LoginPin/>));
