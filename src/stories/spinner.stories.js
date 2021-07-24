48555673bf63563c6d78c2daefb357cca99d97bc
Aman <guptaaman200115@gmail.com>
2021-07-24 20:11:21 +0530

    Replacing ActivityIndicator with svg

--- src/stories/spinner.stories.js
+++ src/stories/spinner.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../components/main-content';
-import {Spinner} from '../components/spinner';
+import Spinner from '../assets/icon/spinner';
 storiesOf('Spinner', module)
   .addDecorator(story => (
       <MainContent style={{ justifyContent: 'center',padding : 10 }}>{story()}</MainContent>
