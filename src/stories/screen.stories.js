4c9fe17b479ee77f9e08e3342718a45a4814fec8
Aman <guptaaman200115@gmail.com>
2021-07-24 23:17:37 +0530

    Adding Loading Screen Story

--- src/stories/screen.stories.js
+++ src/stories/screen.stories.js
@@ -4,9 +4,11 @@ import {Cover} from '../view/cover';
 import ChoosePin from '../view/choose_pin';
 import ConfirmPin from '../view/confirm_pin';
 import LoginPin from '../view/login';
+import Loading from '../view/loading';
 
 storiesOf('Screens', module)
 .add('Cover', () => <Cover/>)
 .add('Choose Pin', () => (<ChoosePin/>))
 .add('Confirm Pin' , () => (<ConfirmPin/>))
-.add('Login Pin' , () => (<LoginPin/>));
+.add('Login Pin' , () => (<LoginPin/>))
+.add('Loading' , () => (<Loading/>));
