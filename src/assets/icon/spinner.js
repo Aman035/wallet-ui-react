ced0a93a700ef0f1b23bde0ae4ed60d564fcce5f
Aman <guptaaman200115@gmail.com>
2021-07-25 19:57:38 +0530

    Svg File Changes

--- src/assets/icon/spinner.js
+++ src/assets/icon/spinner.js
@@ -3,13 +3,11 @@ import React from "react";
 function Spinner(props) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="200"
-      height="200"
+      width="70"
+      height="70"
       display="block"
       preserveAspectRatio="xMidYMid"
       viewBox="0 0 100 100"
-      {...props}
     >
       <path fill="#dedede" d="M10 50a40 40 0 0080 0 40 42 0 01-80 0">
         <animateTransform
