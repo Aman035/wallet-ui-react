e85c08db42acb93c96990970270bd09aa312fc3d
Aman <guptaaman200115@gmail.com>
2021-07-26 21:07:37 +0530

    Custom Preview Configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import Container from '../src/components/container';
+
+addDecorator(story => <Container>{story()}</Container>);
+
+function loadStories() {
+  const context = require.context('../src/stories' , true , /.stories\.js?$/);
+  context.keys().forEach(context);
+}
+
+
+configure(loadStories, module);
