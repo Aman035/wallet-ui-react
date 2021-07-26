25a852b94730af84874320cd44292e8955cf7c20
Aman <guptaaman200115@gmail.com>
2021-07-26 19:01:50 +0530

    Adding icon stories and Style Changes

--- src/stories/icon.stories.js
+++ src/stories/icon.stories.js
@@ -2,11 +2,42 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../components/main-content';
 import MatressIcon from '../assets/img/Logo';
-import Backspace from '../assets/icon/backspace';
+import BackspaceIcon from '../assets/icon/backspace';
+import SettingIcon from '../assets/icon/setting';
+import CorrectIcon from '../assets/icon/correct';
+import RightArrowIcon from '../assets/icon/arrow_right';
+import { color } from '../components/style';
+import {View , StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container : {
+    position : 'absolute' , 
+    top : 0 ,
+    left : 0, 
+    bottom : 0, 
+    right : 0,
+    justifyContent : 'center',
+    alignItems : 'center',
+  },
+
+  container2 : {
+    position : 'absolute' , 
+    top : 0 ,
+    left : 0, 
+    bottom : 0, 
+    right : 0,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : color.blue
+  }
+})
 
 storiesOf('Icons', module)
   .addDecorator(story => (
-      <MainContent style={{ justifyContent: 'center' }}>{story()}</MainContent>
+      <View style={styles.container}>{story()}</View>
   ))
   .add('Matress', () => <MatressIcon/>)
-  .add('Backspace' , () => <Backspace/>);
+  .add('Backspace' , () => <BackspaceIcon/>)
+  .add('Setting' , () => <SettingIcon height={21} width={20}/>)
+  .add('Correct ' ,() => <CorrectIcon/>)
+  .add('Right Arrow' , ()=> (<View style = {styles.container2}><RightArrowIcon/></View>));
