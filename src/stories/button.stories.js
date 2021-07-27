4d020b781c49d3fc883f9fe6d1910c104e82ff7a
Aman <guptaaman200115@gmail.com>
2021-07-27 20:33:33 +0530

    Adding Button Stories

--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import {Button2} from '../components/button';
+import {Button2, Button2_Small , IconButton} from '../components/button';
 import { color } from '../components/style';
 import Button from '../components/button';
 import MainContent from '../components/main-content';
+import SettingIcon from '../assets/icon/setting';
 
 storiesOf('Button', module)
   .addDecorator(story => (
@@ -14,5 +15,6 @@ storiesOf('Button', module)
   .add('Btn1', () => (<Button2 onPress={action('clicked')} style={{backgroundColor : color.orange}}/>))
   .add('Btn1 Disabled', () => (<Button2 onPress={action('clicked')} disabled style={{backgroundColor : color.orange}}/>))
   .add('Btn2', () => (<Button2 onPress={action('clicked')} style={{backgroundColor : color.blue}}/>))
-  .add('Btn2 Disabled', () => (<Button2 onPress={action('clicked')} disabled style={{backgroundColor : color.blue}}/>));
-  
+  .add('Btn2 Disabled', () => (<Button2 onPress={action('clicked')} disabled style={{backgroundColor : color.blue}}/>))
+  .add('Btn2 Small' , () => (<Button2_Small onPress={action('clicked')} style={{backgroundColor : color.blue}}/>) )
+  .add('Icon Button' , () => (<IconButton onPress={action('clicked')}><SettingIcon height={25} width={25}/></IconButton>))
