8454c55fe45bf7b6c654bf5d9b72088aaeb49b00
Aman <guptaaman200115@gmail.com>
2021-08-03 23:58:09 +0530

    Adding Screen Story

--- src/stories/screen.stories.js
+++ src/stories/screen.stories.js
@@ -6,6 +6,7 @@ import ConfirmPin from '../view/confirm_pin';
 import LoginPin from '../view/login';
 import Loading from '../view/loading';
 import Home1 from '../view/home1';
+import Simplified_Home from '../view/simplified_home';
 
 storiesOf('Screens', module)
 .add('Cover', () => <Cover/>)
@@ -14,3 +15,4 @@ storiesOf('Screens', module)
 .add('Login Pin' , () => (<LoginPin/>))
 .add('Loading' , () => (<Loading/>))
 .add('Home', () => (<Home1/>))
+.add('Simplified Home' , () => <Simplified_Home/>)
