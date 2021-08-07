55f29432a11983ec167c42d191233d14723eeab5
Aman <guptaaman200115@gmail.com>
2021-08-07 16:55:24 +0530

    Add Theme State and Dark Theme Colors

---
+++ src/action/theme.js
@@ -0,0 +1,22 @@
+/**
+ * @fileOverview action to handle the overall theme of components
+ */
+
+ class ThemeAction {
+   constructor(store) {
+     this._store = store;
+   }
+
+   // Theme actions
+
+   /**
+    * Changes the theme to light , dark or user defined
+    * @return {undefined}
+    */
+   changeTheme(theme) {
+        if(theme === 'dark' || theme === 'light' || theme === 'user')
+        this._store.theme = theme;
+   }
+}
+ export default ThemeAction;
+ 
---
+++ src/components/color.js
@@ -0,0 +1,39 @@
+/**
+ * @fileOverview Color Themes 
+ * Taken According to Bitcoin Wallet Design Kit
+ */
+
+export const primary = {
+    bitcoin_orange : '#F7931A',
+    bitcoin_blue : '#1C376B',
+    red : '#EB5757',
+    green : '#27AE60',
+    blue : '#2D9CDB',
+    purple : '#BB6BD9'
+}
+
+export const light = {
+    white : '#FFFFFF',
+    neutral1 : '#F8F8F8' ,
+    neutral2 : '#F4F4F4' ,
+    neutral3 : '#EDEDED' ,
+    neutral4 : '#DEDEDE' ,
+    neutral5 : '#BBBBBB' ,
+    neutral6 : '#999999' ,
+    neutral7 : '#777777' , 
+    neutral8 : '#404040' , 
+    black : '#000000'
+}
+
+export const dark = {
+    black : '#000000',
+    neutral1 : '#1A1A1A' ,
+    neutral2 : '#2D2D2D' ,
+    neutral3 : '#444444' ,
+    neutral4 : '#5C5C5C' ,
+    neutral5 : '#787878' ,
+    neutral6 : '#949494' ,
+    neutral7 : '#B0B0B0' , 
+    neutral8 : '#CCCCCC' , 
+    white : '#FFFFFF'
+}
