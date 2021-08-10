4e57061140b2e1492d9775aaa72c34808e46a19f
Aman <guptaaman200115@gmail.com>
2021-08-10 18:40:12 +0530

    Dark theme to Component

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { ScrollView, StyleSheet} from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { dark, light } from "./themeColor";
 
 const styles = StyleSheet.create({
   content: {
     flexGrow: 1,
-    alignItems: 'center'
+    alignItems: "center",
+  },
+
+  //Theme Styles
+  dark: {
+    backgroundColor: dark.black,
+  },
+
+  light: {
+    backgroundColor: light.white,
   },
 });
 
-const MainContent = ({ children, style }) => (
-  <ScrollView contentContainerStyle={[styles.content, style]}>
+const MainContent = ({ children, style, theme = "light" }) => (
+  <ScrollView
+    contentContainerStyle={[
+      styles.content,
+      style,
+      theme === "dark" ? styles.dark : styles.light,
+    ]}
+  >
     {children}
   </ScrollView>
 );
 
 MainContent.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
 
 export default MainContent;
--- src/stories/icon.stories.js
+++ src/stories/icon.stories.js
@@ -1,53 +1,73 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import MainContent from '../components/main-content';
-import MatressIcon from '../assets/img/Logo';
-import BackspaceIcon from '../assets/icon/backspace';
-import SettingIcon from '../assets/icon/setting';
-import CorrectIcon from '../assets/icon/correct';
-import RightArrowIcon from '../assets/icon/arrow_right';
-import SingleBedIcon from '../assets/icon/single_bed';
-import DoubleBedIcon from '../assets/icon/double_bed';
-import TripleBedIcon from '../assets/icon/triple_bed';
-import * as AllFilled from '../assets/icon/filled';
-import * as AllOutlined from '../assets/icon/outline';
-import { color } from '../components/style';
-import {View , StyleSheet} from 'react-native';
-
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import MainContent from "../components/main-content";
+import MatressIcon from "../assets/img/Logo";
+import SingleBedIcon from "../assets/icon/single_bed";
+import DoubleBedIcon from "../assets/icon/double_bed";
+import TripleBedIcon from "../assets/icon/triple_bed";
+import * as AllFilled from "../assets/icon/filled";
+import * as AllOutlined from "../assets/icon/outline";
+import { View, StyleSheet } from "react-native";
 
 const Filled = Object.values(AllFilled);
 const Outline = Object.values(AllOutlined);
 
 const styles = StyleSheet.create({
-  container : {
-    justifyContent : 'center',
-    alignItems : 'center',
-    flexDirection : 'row',
-    flexWrap : 'wrap'
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
 
-  container2 : {
-    justifyContent : 'center',
-    alignItems : 'center',
-    backgroundColor : color.blue,
-    width : '100vh'
+  icon: {
+    height: 100,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  icon : {
-    height : 100,width : 100,
-    alignItems : 'center',
-    justifyContent : 'center'
-  }
-})
+});
 
-storiesOf('Icons', module)
-  .addDecorator(story => (
-      <MainContent style={styles.container}>
-        {story()}
-      </MainContent>
+storiesOf("Icons/light", module)
+  .addDecorator((story) => (
+    <MainContent style={styles.container}>{story()}</MainContent>
   ))
-  .add('Filled Icons' , () => Filled.map((Each ,i)=><View key = {i} style={styles.icon}><Each height="40" width="40"/></View>))
-  .add('Outlined Icons' , () => Outline.map((Each ,i)=><View key = {i} style={styles.icon}><Each height="40" width="40"/></View>))
-  .add('Matress', () => <MatressIcon/>)
-  .add('SingleBed ' ,() => <SingleBedIcon/>)
-  .add('DoubleBed ' ,() => <DoubleBedIcon/>)
-  .add('TripleBed ' ,() => <TripleBedIcon/>);
+  .add("Filled Icons", () =>
+    Filled.map((Each, i) => (
+      <View key={i} style={styles.icon}>
+        <Each height="40" width="40" color="#000"/>
+      </View>
+    ))
+  )
+  .add("Outlined Icons", () =>
+    Outline.map((Each, i) => (
+      <View key={i} style={styles.icon}>
+        <Each height="40" width="40" color="#000"/>
+      </View>
+    ))
+  )
+  .add("Matress", () => <MatressIcon />)
+  .add("SingleBed ", () => <SingleBedIcon />)
+  .add("DoubleBed ", () => <DoubleBedIcon />)
+  .add("TripleBed ", () => <TripleBedIcon />);
+
+storiesOf("Icons/dark", module)
+  .addDecorator((story) => (
+    <MainContent style={styles.container} theme="dark">
+      {story()}
+    </MainContent>
+  ))
+  .add("Filled Icons", () =>
+    Filled.map((Each, i) => (
+      <View key={i} style={styles.icon}>
+        <Each height="40" width="40" color="#fff"/>
+      </View>
+    ))
+  )
+  .add("Outlined Icons", () =>
+    Outline.map((Each, i) => (
+      <View key={i} style={styles.icon}>
+        <Each height="40" width="40" color="#fff"/>
+      </View>
+    ))
+  );
