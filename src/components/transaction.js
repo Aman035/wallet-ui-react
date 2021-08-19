bbf5422e78a5c8c93e809cc5b5efba323cbf191a
Aman <guptaaman200115@gmail.com>
2021-08-19 23:44:20 +0530

    Transaction Component Changes

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,41 +1,71 @@
 import React from 'react';
-import { View,StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+import { font } from './style';
 import { H3Text, H4Text } from './text';
-import {primary} from './themeColor';
+import { primary } from './themeColor';
 
-const Transaction  = ({ type,btc,time,amt,style,colors })=>{
-    console.log(colors);
-    const styles = StyleSheet.create({
-        container : {
-            borderTopColor : colors.neutral3,
-            borderTopWidth : 2,
-            flexDirection : 'row',
-            paddingVertical : 26,
-            marginHorizontal : 30,
-            width : '100%'
-        },
-        text : {
-            color : colors.stroke
-        },
-        secText : {
-            color : colors.neutral7
-        },
-        recText : {
-            color : primary.green
-        }
-    })
-
-    return(
-    <View style={[styles.container , style]}>
-        <View style={{flex : 1  , alignItems : 'flex-start'}}>
-            <H3Text style={styles.text}>{type}</H3Text>
-            <H4Text style={styles.secText}>{time}</H4Text>
-        </View>
-        <View style={{flex : 1 , alignItems : 'flex-end'}}>
-            <H3Text style={type === 'Received' || 'Receiving' ? styles.recText : styles.text}>{btc}</H3Text>
-            <H4Text style={styles.secText}>{amt}</H4Text>
-        </View>
+const Transaction = ({ type, btc, time, amt, style, colors }) => {
+  
+  const styles = StyleSheet.create({
+    container: {
+      borderTopColor: colors.neutral3,
+      borderTopWidth: 1,
+      flexDirection: 'row',
+      paddingVertical: 26,
+      paddingHorizontal: 10,
+      marginHorizontal : 30,
+      width: '92%',
+    },
+    text: {
+      color: colors.stroke,
+    },
+    secText: {
+      color: colors.neutral7,
+      lineHeight : 35,
+      fontSize : 16
+    },
+    recText: {
+      color: primary.green,
+    },
+    flexContainer1 : {
+        flex: 4,
+        alignItems: 'flex-start', 
+        minWidth: 100
+    },
+    flexContainer2 : {
+        flex: 4,
+        alignItems: 'flex-end', 
+        minWidth: 100
+    },
+    picContainer : {
+        paddingRight: 6 
+    }
+  });
+ 
+  return (
+    <View style={[styles.container, style]}>
+      <View style={styles.picContainer}>
+        <Image
+          source={require('../assets/img/profile.png')}
+          style={{ width: 40, height: 40 }}
+        />
+      </View>
+      <View style={styles.flexContainer1}>
+        <H3Text style={styles.text}>{type}</H3Text>
+        <H4Text style={styles.secText}>{time}</H4Text>
+      </View>
+      <View style={styles.flexContainer2}>
+        <H3Text
+          style={
+            (type === 'Received' || type === 'Receiving...') ? styles.recText : styles.text
+          }
+        >
+          {btc}
+        </H3Text>
+        <H4Text style={styles.secText}>{amt}</H4Text>
+      </View>
     </View>
-)};
+  );
+};
 
 export default Transaction;
