22a8ac8e00030fef9d7eb2abc805e2afc5d8e60c
Aman <guptaaman200115@gmail.com>
2021-08-25 22:51:09 +0530

    Cover and home views style changes

--- src/view/cover.js
+++ src/view/cover.js
@@ -4,40 +4,41 @@ import { View, StyleSheet } from 'react-native';
 import MattressLogo from '../assets/icon/myMattress/Logo';
 import { H5Text, H3Text } from '../components/text';
 import MainContent from '../components/main-content';
+import gStyle from '../components/globalStyleSheet';
 
 const Cover = ({ store }) => {
+  const color = store.theme.color;
+  const gstyles = gStyle(color);
   const styles = StyleSheet.create({
-    container: {
+    container1: {
+      flex: 3,
       alignItems: 'center',
-      padding: 20,
-      backgroundColor: store.theme.color.fill,
+      justifyContent: 'center'
     },
-    heading: {
-      paddingVertical: 20,
+    container2: {
+      flex: 1,
+      justifyContent: 'flex-end',
     },
     btn: {
       backgroundColor: store.theme.primary.bitcoin_blue,
-      marginTop: 'auto',
-      marginBottom: 20,
     },
-    text: {
-      color: store.theme.color.stroke,
-    },
-    btnText: {
-      color: store.theme.color.fill,
-    },
+    text : {
+      paddingVertical : 7
+    }
   });
 
   return (
-    <MainContent style={styles.container}>
-      <MattressLogo color={store.theme.color.stroke} />
-      <H5Text style={styles.text}>My Matress</H5Text>
-      <View style={styles.heading}>
-        <H3Text style={styles.text}>Rest in peace.</H3Text>
+    <MainContent style={gstyles.container}>
+      <View style={styles.container1}>
+        <MattressLogo color={color.stroke}/>
+        <H5Text style={gstyles.text}>My Matress</H5Text>
+        <H3Text style={[gstyles.text, styles.text]}>Rest in peace.</H3Text>
       </View>
-      <Button2 style={styles.btn} onPress={() => {}}>
-        <H3Text style={styles.btnText}>Make my bed</H3Text>
-      </Button2>
+      <View style={styles.container2}>
+        <Button2 style={styles.btn} onPress={() => {}}>
+          <H3Text style={gstyles.btnText}>Make my bed</H3Text>
+        </Button2>
+      </View>
     </MainContent>
   );
 };
