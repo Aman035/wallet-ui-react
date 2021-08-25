ff20e0d46273761ce3aee1d6f57d944ac23ed3df
Aman <guptaaman200115@gmail.com>
2021-08-25 22:47:24 +0530

    Component Changes

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -5,49 +5,49 @@ import { H3Text, H4Text } from './text';
 import { primary } from './themeColor';
 
 const Transaction = ({ type, btc, time, amt, style, colors }) => {
-  
   const styles = StyleSheet.create({
     container: {
       borderTopColor: colors.neutral3,
       borderTopWidth: 1,
       flexDirection: 'row',
       paddingVertical: 26,
-      paddingHorizontal: 10,
-      marginHorizontal : 30,
-      width: '92%',
+      width: '100%',
     },
     text: {
+      textAlign: 'right',
       color: colors.stroke,
     },
     secText: {
       color: colors.neutral7,
-      lineHeight : 35,
-      fontSize : 16
+      lineHeight: 35,
+      fontSize: 15,
+      textAlign: 'left',
     },
     recText: {
+      textAlign: 'right',
       color: primary.green,
     },
-    flexContainer1 : {
-        flex: 4,
-        alignItems: 'flex-start', 
-        minWidth: 100
+    flexContainer1: {
+      flex: 4,
+      alignItems: 'flex-start',
+      minWidth: 100,
     },
-    flexContainer2 : {
-        flex: 4,
-        alignItems: 'flex-end', 
-        minWidth: 100
+    flexContainer2: {
+      flex: 4,
+      alignItems: 'flex-end',
+      minWidth: 100,
     },
-    picContainer : {
-        paddingRight: 6 
-    }
+    picContainer: {
+      paddingRight: 4,
+    },
   });
- 
+
   return (
     <View style={[styles.container, style]}>
       <View style={styles.picContainer}>
         <Image
           source={require('../assets/img/profile.png')}
-          style={{ width: 40, height: 40 }}
+          style={{ width: 35, height: 35 }}
         />
       </View>
       <View style={styles.flexContainer1}>
@@ -57,10 +57,12 @@ const Transaction = ({ type, btc, time, amt, style, colors }) => {
       <View style={styles.flexContainer2}>
         <H3Text
           style={
-            (type === 'Received' || type === 'Receiving...') ? styles.recText : styles.text
+            type === 'Received' || type === 'Receiving...'
+              ? styles.recText
+              : styles.text
           }
         >
-          <BitcoinIcon color={colors.stroke} height={20}/>
+          <BitcoinIcon color={colors.stroke} height={20} width={20} />
           {btc}
         </H3Text>
         <H4Text style={styles.secText}>{amt}</H4Text>
