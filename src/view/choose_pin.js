7c4e8dee20376d787357ac11ce1c124f30e9b193
Aman <guptaaman200115@gmail.com>
2021-08-25 22:50:01 +0530

    Pin Views Flex Style

--- src/view/choose_pin.js
+++ src/view/choose_pin.js
@@ -4,44 +4,46 @@ import { observer } from 'mobx-react';
 import MainContent from '../components/main-content';
 import { H3Text, H6Text } from '../components/text';
 import { PinSecret, PinPad } from '../components/pin';
-
-//choose Pin View
+import gStyle from '../components/globalStyleSheet';
 
 const ChoosePin = ({ store, auth }) => {
+  const color = store.theme.color;
+  const gstyles = gStyle(color);
   const styles = StyleSheet.create({
-    content: {
-      paddingLeft: 20,
-      paddingRight: 20,
-      backgroundColor: store.theme.color.fill,
+    container1: {
+      flex: 1,
     },
-    title: {
-      marginTop: 50,
-      color: store.theme.color.stroke,
+    container2: {
+      flex: 2,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
     },
-    text: {
-      marginTop: 10,
-      textAlign: 'center',
-      color: store.theme.color.neutral7,
-    },
-    pinwrap: {
-      paddingTop: 30,
-      paddingBottom: 60,
+    secText: {
+      paddingVertical: 20,
     },
   });
 
   return (
-    <MainContent style={styles.content}>
-      <H6Text style={styles.title}>Choose a PIN</H6Text>
-      <H3Text style={styles.text}>Make Sure you can remember it.</H3Text>
-      <View style={styles.pinwrap}>
-        <PinSecret pin={store.auth.newPin} stroke={store.theme.color.stroke} border={store.theme.color.stroke}/>
+    <MainContent style={gstyles.container}>
+      <View style={[gstyles.align, styles.container1]}>
+        <H6Text style={gstyles.text}>Choose a PIN</H6Text>
+        <H3Text style={[gstyles.secText, styles.secText]}>
+          Make Sure you can remember it.
+        </H3Text>
+        <PinSecret
+          pin={store.auth.newPin}
+          stroke={color.stroke}
+          border={color.stroke}
+        />
       </View>
-      <PinPad
-        onInput={(digit) => auth.pushPinDigit({ digit, param: 'newPin' })}
-        onBackspace={() => auth.popPinDigit({ param: 'newPin' })}
-        stroke={store.theme.color.stroke}
-        secondary={store.theme.color.neutral7}
-      />
+      <View style={styles.container2}>
+        <PinPad
+          onInput={(digit) => auth.pushPinDigit({ digit, param: 'newPin' })}
+          onBackspace={() => auth.popPinDigit({ param: 'newPin' })}
+          stroke={color.stroke}
+          secondary={color.neutral7}
+        />
+      </View>
     </MainContent>
   );
 };
