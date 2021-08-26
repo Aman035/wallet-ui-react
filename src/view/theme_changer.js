262933d7b9e7db8533a26d7290d9eb125eaaefe6
Aman <guptaaman200115@gmail.com>
2021-08-26 16:13:00 +0530

    View Style Changes

--- src/view/theme_changer.js
+++ src/view/theme_changer.js
@@ -1,42 +1,45 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { observer } from 'mobx-react';
 import MainContent from '../components/main-content';
 import { H3Text, H5Text } from '../components/text';
-import { Button } from '../components/button';
+import { Button2 } from '../components/button';
+import gStyle from '../components/globalStyleSheet';
 
 const ThemeChanger = ({ store, theme }) => {
+  const color = store.theme.color;
+  const gstyles = gStyle(color);
+
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: store.theme.color.fill,
-      paddingVertical: 20,
-    },
-    text: {
-      color: store.theme.color.stroke,
+      flex: 1
     },
     btn: {
-      marginVertical: 30,
-      borderRadius: 5,
       backgroundColor: store.theme.primary.bitcoin_orange,
-      width: '100%',
-      padding: 2,
+      marginVertical : 5
     },
   });
 
   return (
-    <MainContent style={styles.container}>
-      <H5Text style={styles.text}>Theme Changer</H5Text>
-      <MainContent style={{ justifyContent: 'center' }}>
-        <Button style={styles.btn} onPress={() => theme.lightTheme()}>
-          <H3Text style={styles.text}>Light</H3Text>
-        </Button>
-        <Button style={styles.btn} onPress={() => theme.darkTheme()}>
-          <H3Text style={styles.text}>Dark</H3Text>
-        </Button>
-        <Button disabled style={styles.btn} onPress={() => theme.userTheme()}>
-          <H3Text style={styles.text}>User Defined (Coming Soon)</H3Text>
-        </Button>
-      </MainContent>
+    <MainContent style={gstyles.container}>
+      <View style={[gstyles.align, styles.container]}>
+        <H5Text style={gstyles.text}>Theme Changer</H5Text>
+      </View>
+      <View style={[gstyles.align, styles.container]}>
+        <Button2 style={styles.btn} onPress={() => theme.lightTheme()}>
+          <H3Text style={gstyles.text}>Light</H3Text>
+        </Button2>
+      </View>
+      <View style={[gstyles.align, styles.container]}>
+        <Button2 style={styles.btn} onPress={() => theme.darkTheme()}>
+          <H3Text style={gstyles.text}>Dark</H3Text>
+        </Button2>
+      </View>
+      <View style={[gstyles.align, styles.container]}>
+        <Button2 disabled style={styles.btn} onPress={() => theme.userTheme()}>
+          <H3Text style={gstyles.text}>User Defined (Coming Soon)</H3Text>
+        </Button2>
+      </View>
     </MainContent>
   );
 };
