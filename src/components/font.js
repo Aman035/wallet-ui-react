b205a09c75c213d7a06fc7be6e530ed7ab3f2f87
Aman <guptaaman200115@gmail.com>
2021-09-06 17:07:02 +0530

    Adding font stories and styles

--- src/components/font.js
+++ src/components/font.js
@@ -1,5 +1,7 @@
-import '../assets/font/Inter-Medium.ttf';
+import '../assets/font/Inter-Thin.ttf';
 import '../assets/font/Inter-Regular.ttf';
+import '../assets/font/Inter-Medium.ttf';
 import '../assets/font/Inter-SemiBold.ttf';
+import '../assets/font/Inter-Bold.ttf';
 import '../assets/font/PaytoneOne-Regular.ttf';
 import './font.css';
